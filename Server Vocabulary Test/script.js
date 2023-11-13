6c26cab81fc82c034ca306c0206daf33045fec60
Kyrylo Savchenko <dzheis@gmail.com>
2023-11-13 21:53:44 +0000

    Начал переписывать на tailwindcss

--- Server Vocabulary Test/script.js
+++ Server Vocabulary Test/script.js
@@ -1,142 +1,146 @@
-"Use strict"
+"use strict";
 
-const buttonContainer = document.getElementById('buttonContainer');
+// Получение элементов интерфейса
 const forwardBtn = document.getElementById('forwardBtn');
 const backBtn = document.getElementById('backBtn');
 const randomBtn = document.getElementById('randomBtn');
 const toggleButton = document.getElementById('toggleButton');
-const toggleRu = document.getElementById('toggleRu');
 const englishWord = document.getElementById('englishWordClick');
 const russianWord = document.getElementById('russianWordClick');
+const searchWordInput = document.getElementById('searchWordInput');
+const searchWordButton = document.getElementById('searchWordButton');
+const addSearchedWordButton = document.getElementById('addSearchedWordButton');
 
 const apiUrl = 'https://vocabulary-e561acf67931.herokuapp.com';
-let vocabulary = {}
-
-let currenIndex = 0;
+let vocabulary = {};
+let currentIndex = 0;
 let previousIndex = null;
-let showInputs = false;
+let searchedWordData = null;
 
+// Функция для определения языка слова
+function getTranslationLanguage(word) {
+    if (/[\u0400-\u04FF]/.test(word)) {
+        return 'ru-en'; // Направление перевода с русского на английский
+    } else {
+        return 'en-ru'; // Направление перевода с английского на русский
+    }
+}
+
+// Функция для отображения слова на экране
 function showWord(index) {
     let englishWords = Object.keys(vocabulary);
     let wordKey = englishWords[index];
     let word = vocabulary[wordKey];
     englishWord.textContent = wordKey;
     russianWord.textContent = word;
-};
+}
+
+// Загрузка слов из сервера
 function fetchWordsFromServer() {
     console.log("Запрос к серверу отправлен");
     fetch(`${apiUrl}/words`)
         .then(response => {
             if (!response.ok) {
-                throw new Error('Cетевой ответ не ok');
+                throw new Error('Сетевой ответ не ok');
             }
             return response.json();
         })
         .then(data => {
+            vocabulary = {};
             data.forEach(word => {
                 vocabulary[word.english] = word.russian;
             });
-            // После загрузки слов вы можете, например, обновить отображение на странице
-            showWord(currenIndex);
-
+            showWord(currentIndex);
         })
         .catch(error => {
-            console.error("Ошибка при загрузке слов:", error)
-            alert(`Ошибка: ${error}`)
+            console.error("Ошибка при загрузке слов:", error);
         });
 }
 fetchWordsFromServer();
 
+// Обработчики событий для кнопок навигации
 randomBtn.addEventListener('click', function () {
-    previousIndex = currenIndex;
+    previousIndex = currentIndex;
     let englishWords = Object.keys(vocabulary);
     let randomIndex = Math.floor(Math.random() * englishWords.length);
-    currenIndex = randomIndex;
-    showWord(currenIndex);
+    currentIndex = randomIndex;
+    showWord(currentIndex);
+});
 
-});
 backBtn.addEventListener('click', function () {
-    let temp = currenIndex;
-    currenIndex = previousIndex;
-    previousIndex = temp;
-    showWord(currenIndex);
+    if (previousIndex !== null) {
+        currentIndex = previousIndex;
+        showWord(currentIndex);
+    }
 });
+
 forwardBtn.addEventListener('click', function () {
     let englishWords = Object.keys(vocabulary);
-    if (currenIndex === englishWords.length - 1) {
-        currenIndex = 0;
-    } else {
-        currenIndex++;
-    }
-    showWord(currenIndex);
+    currentIndex = (currentIndex + 1) % englishWords.length;
+    showWord(currentIndex);
 });
+
 toggleButton.addEventListener('click', function () {
-    if (russianWord.classList.contains('visible')) {
-        russianWord.classList.remove('visible');
-        russianWord.classList.add('hidden');
-        toggleButton.innerText = "Показать перевод"
-    } else {
-        russianWord.classList.remove('hidden');
-        russianWord.classList.add('visible');
-        toggleButton.innerText = "Скрыть перевод"
-    }
+    russianWord.classList.toggle('visible');
+    russianWord.classList.toggle('hidden');
+    toggleButton.innerText = russianWord.classList.contains('visible') ? "Скрыть перевод" : "Показать перевод";
 });
-function toggleInputs() {
-    showInputs = !showInputs;
-    if (showInputs) {
-        document.querySelector(".inputContainer").classList.contains('nonVisible');
-        document.querySelector(".inputContainer").classList.remove('nonVisible');
-        document.querySelector(".inputContainer").classList.add('visible');
-        document.querySelector("#toggleButtonInput").textContent = 'Скрыть поля ввода';
-    } else {
-        document.querySelector(".inputContainer").classList.contains('visible');
-        document.querySelector(".inputContainer").classList.remove('visible');
-        document.querySelector(".inputContainer").classList.add('nonVisible');
-        document.querySelector("#toggleButtonInput").textContent = 'Показать поля ввода';
-    }
-};
-document.getElementById('wordForm').addEventListener('submit', function (e) {
-    e.preventDefault(); // Отменяем стандартное поведение формы
 
-    // Получаем значения из полей ввода
-    const englishWord = document.getElementById('englishWord').value.trim(); // используем trim() для удаления лишних пробелов
-    const russianWord = document.getElementById('russianWord').value.trim();
-
-    // Проверка, что поля не пустые
-    if (!englishWord || !russianWord) {
-        // Если хотя бы одно поле пустое, показываем ошибку
-        alert('Пожалуйста, введите слово правильно!');
-        return; // прерываем функцию, чтобы не отправлять запрос на сервер
+// Обработчик поиска слова в Yandex
+searchWordButton.addEventListener('click', function () {
+    const wordToSearch = searchWordInput.value.trim();
+    if (!wordToSearch) {
+        alert('Пожалуйста, введите слово для поиска');
+        return;
     }
 
-    // Если проверка прошла успешно, продолжаем отправку данных на сервер
-    fetch(`${apiUrl}/add-word`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            english: englishWord,
-            russian: russianWord
+    const translationDirection = getTranslationLanguage(wordToSearch);
+
+    fetch(`https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=dict.1.1.20231112T183924Z.d0433efb3942c6ed.a9f789cb257dbe73aed3b64b45e24cb94a0ecd7c&lang=${translationDirection}&text=${wordToSearch}`)
+        .then(response => response.json())
+        .then(data => {
+            if (data.def && data.def.length > 0) {
+                const wordInfo = data.def[0];
+                const translation = wordInfo.tr.map(tr => tr.text).join(', ');
+                searchedWordData = {
+                    [translationDirection.startsWith('en') ? 'english' : 'russian']: wordToSearch,
+                    [translationDirection.startsWith('en') ? 'russian' : 'english']: translation
+                };
+                alert(`${wordToSearch} - ${translation}`);
+            } else {
+                alert('Слово не найдено');
+                searchedWordData = null;
+            }
         })
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.success) {
-            alert('Слово успешно добавлено!');
-            fetchWordsFromServer(); // Это должна быть ваша функция для обновления списка слов на странице
-            document.getElementById('wordForm').reset(); // Сброс формы
-        } else {
-            alert('Произошла ошибка при добавлении слова.');
-        }
-    })
-    .catch(error => {
-        console.error('Ошибка:', error);
-    });
+        .catch(error => {
+            console.error('Ошибка при запросе:', error);
+            searchedWordData = null;
+        });
 });
 
-showWord(currenIndex);
-
-
-
-
+// Обработчик для добавления слова в базу данных
+addSearchedWordButton.addEventListener('click', function () {
+    if (searchedWordData) {
+        fetch(`${apiUrl}/add-word`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(searchedWordData)
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data.success) {
+                    alert('Слово успешно добавлено в базу данных');
+                    fetchWordsFromServer(); // Обновляем список слов
+                } else {
+                    alert('Ошибка при добавлении слова в базу данных');
+                }
+            })
+            .catch(error => {
+                console.error('Ошибка:', error);
+            });
+    } else {
+        alert('Нет данных для добавления');
+    }
+});
--- server.js
+++ server.js
@@ -1,50 +1,54 @@
+// Подключение необходимых модулей
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
 const mongoose = require('mongoose');
-require('dotenv').config();
-const Word = require('./models/word');
+require('dotenv').config(); // Подключение файла .env для окружающих переменных
+const Word = require('./models/word'); // Подключение модели слова
 
+const app = express(); // Создание экземпляра приложения Express
 
-const app = express();
-
-// Разрешить доступ всем доменам
+// Разрешение CORS (Cross-Origin Resource Sharing) - разрешение доступа к ресурсам с разных доменов
 app.use(cors());
 
+// Указание директории для статических файлов
 app.use(express.static(path.join(__dirname, 'Server Vocabulary Test')));
-// Подключаем body-parser
+
+// Подключение парсера JSON для обработки тела запросов
 app.use(bodyParser.json());
 
-// Подключаемся к MongoDB
+// Подключение к MongoDB с использованием окружающей переменной MONGO_URL из файла .env
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
+
+// Обработка GET-запроса для получения списка слов
 app.get('/words', async (req, res) => {
     try {
-        const words = await Word.find();
-        res.json(words);
+        const words = await Word.find(); // Извлечение всех слов из базы данных
+        res.json(words); // Отправка списка слов в формате JSON
     } catch (error) {
-        res.status(500).json({ message: 'Server error' });
+        res.status(500).json({ message: 'Server error' }); // Обработка ошибок сервера
     }
 });
 
-// Обрабатываем POST-запрос для добавления слова
+// Обработка POST-запроса для добавления нового слова
 app.post('/add-word', async (req, res) => {
     try {
         const newWord = new Word({
-            english: req.body.english,
-            russian: req.body.russian
+            english: req.body.english, // Извлечение английского слова из тела запроса
+            russian: req.body.russian  // Извлечение русского слова из тела запроса
         });
-        await newWord.save();
-        res.json({ success: true });
+        await newWord.save(); // Сохранение нового слова в базе данных
+        res.json({ success: true }); // Отправка успешного ответа в формате JSON
     } catch (error) {
-        res.status(500).json({ success: false, message: 'Server error' });
+        res.status(500).json({ success: false, message: 'Server error' }); // Обработка ошибок сервера
     }
 });
 
-// Запускаем сервер
+// Запуск сервера на указанном порту из окружающей переменной PORT
 app.listen(process.env.PORT, () => {
     console.log(`Сервер запущен на порту ${process.env.PORT}`);
 });
