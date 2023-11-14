00a14fafdae27653392f3d9ea5fce112c49bfc08
Kyrylo Savchenko <dzheis@gmail.com>
2023-11-14 00:20:38 +0000

    Переписал свой css на tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,17 @@ module.exports = {
       fontFamily: {
         'sans': ['Roboto', 'sans-serif'],
       },
+      backgroundColor: {
+        'f4edea': '#f4edea',
+      },
       backgroundImage: {
         'custom-image': "url('../img/christopher-gower-vjMgqUkS8q8-unsplash.jpg')",
+      },
+      width: {
+        '1000': '1000px',
+      },
+      height: {
+        '450': '450px',
       },
     },
   },
